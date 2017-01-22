557ab070931921b2ad27240aa0781534fff6360b
Jan Fredrik Håvie <janfredrik@havie.net>
2017-01-22 15:32:21 +0100

    changed db setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users = User.create([{email: "janfredrik@havie.net"}, {global_admin: :true}, {created_at: Time.now}, {updated_at: Time.now}, {encrypted_password: ""}]);
+users = User.create([{email: "janfredrik@havie.net", global_admin: :true, created_at: Time.now, updated_at: Time.now, encrypted_password: ""}]);
